d4965fb6de03de14248be93be7cf14cd314a634c
Deepak <dkofficial153@gmail.com>
2025-08-17 19:06:51 +0530

    varialbes typecasting

--- variables.py
+++ variables.py
@@ -1,2 +1,12 @@
-hello = "hello bro"
-print(hello)
+# Casting variables
+s = "10"  # Initially a string
+n = int(s)  # Cast string to integer
+cnt = 5
+f = float(cnt)  # Cast integer to float
+age = 25
+s2 = str(age)  # Cast integer to string
+
+# Display results
+print(n)
+print(f)
+print(s2)
