57fc864e61bbda198ba2324155f28a41fb545c70
Deepak <dkofficial153@gmail.com>
2025-09-12 05:24:55 +0530

    functions and helloworld learned

---
+++ structural/functions.py
@@ -0,0 +1,16 @@
+"""SPECIAL CHARACTERS 
+\n - goes to new line
+\t - tab   
+"""
+
+print('---------------------------------')
+print("...ESCAPE SEQUENCE...")
+print('---------------------------------')
+
+print("HI \"PYTHON\" ")    #   \"PYTHON\" IS A MAIN CONTENT 
+print('HI "PYTHON"') # SECOND METHOD TO MAIN CONTENT 
+
+print("path : c:\\users\\Deepak\n") #backslash is special character so we want to print it we used double backslash to print each 
+
+print("hi da\n") #leave a line space between two statements 
+print("HI")
---
+++ structural/hellowold.py
@@ -0,0 +1,5 @@
+print('---------------------------------')
+print("LEARN PYTHON")
+print('---------------------------------')
+
+print("Hello pythoners welcome")
