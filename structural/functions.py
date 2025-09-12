c738a18edc99213b3241342a5d9ce209f1098283
Deepak <dkofficial153@gmail.com>
2025-09-12 05:30:36 +0530

    tab space command updated

--- structural/functions.py
+++ structural/functions.py
@@ -5,12 +5,18 @@
 
 print('---------------------------------')
 print("...ESCAPE SEQUENCE...")
+print('---------------------------------\n')
+print('---------------------------------')
+print("NEW LINE")
 print('---------------------------------')
 
-print("HI \"PYTHON\" ")    #   \"PYTHON\" IS A MAIN CONTENT 
-print('HI "PYTHON"') # SECOND METHOD TO MAIN CONTENT 
-
+print("HI \"PYTHON\"\n")    #   \"PYTHON\" IS A MAIN CONTENT 
+print('HI "PYTHON"\n') # SECOND METHOD TO MAIN CONTENT 
 print("path : c:\\users\\Deepak\n") #backslash is special character so we want to print it we used double backslash to print each 
+print("hi da\n") #create a  new line space between two statements 
+print("HI\n")
+print('---------------------------------')
+print("TAB SPACE")
+print('---------------------------------')
 
-print("hi da\n") #leave a line space between two statements 
-print("HI")
+print("message1\tmessage2")   #IT create space between two messages 
