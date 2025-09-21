c1e4652b736ea510c8be058a25e3c9a0fbedb643
Deepak <dkofficial153@gmail.com>
2025-09-21 23:02:18 +0530

    typcasting working details

---
+++ structural/typecasting.py
@@ -0,0 +1,18 @@
+x="20"
+y='30'
+print(type(x))
+print(type(y))
+
+#typecasting 
+
+print(int(x))
+print(int(y))
+
+#adding via typcasting return string addition 
+print(int(x)+int(y))
+ 
+#without typcasting return  the values of declared 
+print(x+y) # it returns combine of both x+y as a string 2030
+
+
+
