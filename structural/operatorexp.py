7985aa143a94b2b77b9678388d42f8f4d7e3082b
Deepak <dkofficial153@gmail.com>
2025-09-22 12:40:58 +0530

    the python

---
+++ structural/operatorexp.py
@@ -0,0 +1,29 @@
+#arithmetic operators 
+a=2
+b=3
+print(a+b) #add
+print(a-b) #sub
+print(a*b) #mul
+print(a/b) #div  results quotient
+print(a ** b)  #2*2*2
+print(a  % b)  #modulus results remainder
+print(a // b)  # result quotient in whole number 
+
+#comparison operators 
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+#logical operators 
+
+x= True
+y= False
+print(x and y)
+print(x or y)
+print(not y)
+
+
+
+
