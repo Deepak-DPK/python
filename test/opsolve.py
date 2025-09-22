0b64422480aeee291497b5b60c752c5cc96ddf9d
Deepak <dkofficial153@gmail.com>
2025-09-22 13:05:52 +0530

    gst discount problem examp

--- test/opsolve.py
+++ test/opsolve.py
@@ -1,10 +1,11 @@
 """problem statement
-u have an amount discount 18% gst tax to  the amount and 10 percent to total amount 
+u have an amount add 18% gst tax to  the amount and 10 percent discount to total  amount with tax
 """
 
 amount = 1200
 tax= amount * 0.18
 total = amount + tax
+print(total)
 
 if total > 1000:
     discount=total * 0.10
