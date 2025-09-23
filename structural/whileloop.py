169ddd5e78fcd69051d277d7b217297a8bf15718
Deepak <dkofficial153@gmail.com>
2025-09-23 17:57:55 +0530

    while clear understadning

--- structural/whileloop.py
+++ structural/whileloop.py
@@ -1,6 +1,11 @@
-correct_pin='1234'
-entered_pin=" "
+# A simple countdown
+number = 5
 
-while entered_pin!=correct_pin:
-    entered_pin=input("enter the correct pin: ")
-print("access granted")
+print("Starting countdown!")
+
+# This loop will run as long as 'number' is greater than 0
+while number > 0:
+    print(number)
+    number = number - 1 # This is crucial to avoid an infinite loop!
+
+print("Blast off! 🚀")
