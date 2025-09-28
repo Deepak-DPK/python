0a028b555a981a48e37cbe4b09a58e059bc2d270
Deepak <dkofficial153@gmail.com>
2025-09-28 19:34:17 +0530

    tuple list diff

---
+++ structural/tuple.py
@@ -0,0 +1,15 @@
+# ======================================================================================================
+#                      LIST                      |                      TUPLE
+# ======================================================================================================
+# Syntax: Square brackets []                     |  Syntax: Round brackets ()
+# Example: my_list = [1, "a"]                    |  Example: my_tuple = (1, "a")
+# ------------------------------------------------------------------------------------------------------
+# Mutability: Mutable - can be changed after     |  Mutability: Immutable - cannot be changed
+#             creation.                          |                              after creation.
+# ------------------------------------------------------------------------------------------------------
+# Performance: Slightly slower due to its        |  Performance: Faster and more memory-
+#              dynamic size.                     |               efficient due to its fixed size.
+# ------------------------------------------------------------------------------------------------------
+# Use Case: For a collection that needs to       |  Use Case: For data that should never
+#           change (e.g., a to-do list).         |              change (e.g., coordinates).
+# ======================================================================================================
