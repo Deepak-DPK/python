c96bf06103ac812b792bc3bc688911aa739638b2
Deepak <dkofficial153@gmail.com>
2025-10-02 18:17:06 +0530

    reverse a string

--- problems/revstrg.py
+++ problems/revstrg.py
@@ -1,21 +1,27 @@
-# # # ==================================================
-# # #           REVERSE A STRING USING SLICING 
-# # # ==================================================
+# ==================================================
+#           REVERSE A STRING USING SLICING 
+# ==================================================
 
-# # def rev_strg(s):
-# #     return s[::-1]
+def rev_strg(s):
+    return s[::-1]
 
-# # org_str=input()
-# # revstrg=rev_strg(org_str)
-# # print(revstrg)
+org_str=input()
+revstrg=rev_strg(org_str)
+print(revstrg)
 
-# # ==================================================
-# #           REVERSE A STRING USING JOIN 
-# # ==================================================
 
-# def rev_strg(s):
-#     return "".join(reversed(s))
-# org_str=input()
-# res=rev_strg(org_str)
-# print(res)
+if org_str == revstrg:
+    print(" IT IS A PALINDROME ")
+else:
+    print("It is not a palindrome ")
+
+# ==================================================
+#           REVERSE A STRING USING JOIN 
+# ==================================================
+
+def rev_strg(s):
+    return "".join(reversed(s))
+org_str=input()
+res=rev_strg(org_str)
+print(res)
 
