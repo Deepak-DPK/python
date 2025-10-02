590375721ef1d28b86de1a0b0ef0899fa2408c60
Deepak <dkofficial153@gmail.com>
2025-10-02 11:09:49 +0530

    prime number

---
+++ problems/prime.py
@@ -0,0 +1,9 @@
+#Prime Number 
+num=int(input('enter your number: '))
+
+for prime in range(2,num):
+    if num % prime == 0:
+        print(f"{num}is not a prime number")
+        break
+else:
+    print(f"{num} Is a prime number")
