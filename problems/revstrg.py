4dd8afefbde3240673925e186d04ecd2b97920fe
Deepak <dkofficial153@gmail.com>
2025-10-02 12:33:44 +0530

    reverse the string

---
+++ problems/revstrg.py
@@ -0,0 +1,21 @@
+# # # ==================================================
+# # #           REVERSE A STRING USING SLICING 
+# # # ==================================================
+
+# # def rev_strg(s):
+# #     return s[::-1]
+
+# # org_str=input()
+# # revstrg=rev_strg(org_str)
+# # print(revstrg)
+
+# # ==================================================
+# #           REVERSE A STRING USING JOIN 
+# # ==================================================
+
+# def rev_strg(s):
+#     return "".join(reversed(s))
+# org_str=input()
+# res=rev_strg(org_str)
+# print(res)
+
