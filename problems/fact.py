0a55387501d7c3bc7fa959a1cff5e981446a873d
Deepak <dkofficial153@gmail.com>
2025-10-03 10:45:31 +0530

    factorial

---
+++ problems/fact.py
@@ -0,0 +1,15 @@
+def fact(n):
+    if n<0:
+        return "enter positive number"
+    elif n==0:
+        return 1
+    else:
+        result=1
+        for i in range(1,n+1):
+            result =result * i
+        return result
+ 
+# num=int(input("Enter the number: "))
+num=5
+result = fact(num)
+print(f"the factorial of {num} is {result}")
