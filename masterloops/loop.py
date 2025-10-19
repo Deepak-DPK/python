af37798f9a54f22fd2a9866089cfe8cea2a15eb4
Deepak <dkofficial153@gmail.com>
2025-10-19 19:16:33 +0530

    Step 1 understanding what loops really are

---
+++ masterloops/loop.py
@@ -0,0 +1,29 @@
+
+# 🌱 Step 1: Understanding What Loops Really Are
+"""
+Lets start simple — what is a loop?
+
+A loop is a way to repeat a set of instructions multiple times without rewriting the code again and again.
+
+🧠 Real-life Analogy
+
+Imagine you are sending a “Thank You” message to 100 customers after they make a purchase.
+
+"""
+# Withour Loop-------------------------------------------------------------------------------#
+
+print("Thank you, Customer 1!")
+print("Thank you, Customer 2!")
+print("Thank you, Customer 3!")
+print("-----------------------")
+# ... imagine doing this 100 times 😩
+#--------------------------------------------------------------------------------------------#
+
+# With Loop----------------------------------------------------------------------------------#
+
+for i in range(1,101):
+    print(f"Hello Welcome {i}")
+
+#💡 This is how loops save your time and make your code clean and efficient.
+#--------------------------------------------------------------------------------------------#
+
