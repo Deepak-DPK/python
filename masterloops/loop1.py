7a43e93b067187d7f384041ed656ae1e172f2123
Deepak <dkofficial153@gmail.com>
2025-10-25 10:50:00 +0530

    looping

--- masterloops/loop1.py
+++ masterloops/loop1.py
@@ -60,6 +60,6 @@ print("*************************************************************************
 print("Print the square of numbers from 1 to 5.")
 
 for i in range(1,6):
-    square=i*2  # or i * i
+    square=i**2  # or i * i
     print(square)
 
